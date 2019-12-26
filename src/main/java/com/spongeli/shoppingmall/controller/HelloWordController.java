bf7c4c39416cb50a2247dd56962943ead9f4e267
仰望天空 <17600484122>
2019-12-26 11:45:04 +0800

    增加日志框架
    Signed-off-by: 仰望天空 <17600484122>

--- src/main/java/com/spongeli/shoppingmall/controller/HelloWordController.java
+++ src/main/java/com/spongeli/shoppingmall/controller/HelloWordController.java
@@ -2,9 +2,10 @@ package com.spongeli.shoppingmall.controller;
 
 import com.spongeli.shoppingmall.common.system.BaseController;
 import com.spongeli.shoppingmall.common.system.CommonResponse;
-import com.spongeli.shoppingmall.pojo.dao.MallUserMapper;
-import com.spongeli.shoppingmall.pojo.model.MallUser;
 import com.spongeli.shoppingmall.service.webservice.index.MallUserService;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/user")
 public class HelloWordController extends BaseController {
+    private static final Logger logger = LogManager.getLogger(HelloWordController.class);
     @Autowired
     private MallUserService service;
 
     @GetMapping("/all")
      public CommonResponse helloWord(){
+        logger.info("你好：{}","log4j2");
         return instanceSuccess( service.query());
     }
 }
